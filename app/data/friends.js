17e1540c66da560262bacd91d781d59958ab0a92
Raul Gonzalez <rgonz166@gmail.com>
2019-09-23 22:53:44 -0700

    fixed questions from survey to work

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,6 +7,11 @@ var users = [{
     name: "Frank",
     photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
     scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+},
+{
+    name: "George",
+    photo: "https://cms-tc.pbskids.org/global/show-icons/square/curious-george.png?mtime=20160722174222",
+    scores: [1, 2, 3, 4, 5, 4, 3, 2, 1, 2]
 }];
 
 module.exports = users;
